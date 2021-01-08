5621b96039025c5323659c52e587081f3774eb68
ngkaizhe <kaizhe1991@hotmail.com>
2021-01-08 21:27:33 +0800

    change vertex shader and fragment shader

--- Assets/shaders/fragment.fs.glsl
+++ Assets/shaders/fragment.fs.glsl
@@ -1,12 +1,9 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoords;
-
-uniform sampler2D texture_diffuse1;
+uniform vec3 color;
 
 void main()
 {    
-    FragColor = vec4(vec3(1.0), 1.0);
-    //FragColor = texture(texture_diffuse1, TexCoords);
+    FragColor = vec4(color, 1.0);
 }
